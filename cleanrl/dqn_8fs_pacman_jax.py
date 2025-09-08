19ec8d76e4f3adcc11a64c36575c18f60dae3869
nerdlab53 <retr0sushi.04@gmail.com>
2025-09-08 15:39:18 +0530

    wandb fix

--- cleanrl/dqn_8fs_pacman_jax.py
+++ cleanrl/dqn_8fs_pacman_jax.py
@@ -149,6 +149,7 @@ if __name__ == "__main__":
             sync_tensorboard=True,
             config=vars(args),
             name=run_name,
+            monitor_gym=True,
             save_code=True,
         )
     writer = SummaryWriter(f"runs/{run_name}")
